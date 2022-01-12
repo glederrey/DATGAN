2985f8caa8db4587a1bf71c73c81a2cf3a29858c
glederrey <gael.lederrey@epfl.ch>
2022-01-12 16:30:45 +0100

    [UPD] some stuff for the Pypi library

---
+++ datgan/__init__.py
@@ -0,0 +1,16 @@
+""" Top-level package for DATGAN """
+
+__author__ = """Gael Lederrey - TRANSP-OR laboratory @ EPFL"""
+__email__ = 'gael.lederrey@epfl.ch'
+__version__ = '1.0'
+
+from datgan.datgan import DATGAN
+from datgan.evaluation.statistical_assessments import stats_assessment
+from datgan.evaluation.ml_efficacy import ml_assessment, transform_results
+
+__all__ = {
+    'DATGAN',
+    'stats_assessment',
+    'ml_assessment',
+    'transform_results'
+}
---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
+
+install_requires = [
+    'numpy>=1.16.0',
+    'pandas>=0.23.4',
+    'scikit-learn>=0.20.2',
+    'tensorflow>=1.13.0, <2.0',
+    'tensorpack==0.9.4',
+]
+
+setup(
+    author="Gael Lederrey - TRANSP-OR laboratory @ EPFL",
+    author_email='gael.lederrey@epfl.ch',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Generative adversarial network with integrated expert knowledge for synthesizing tabular data",
+    entry_points={
+        'console_scripts': [
+            'datgan=datgan.cli:main'
+        ]
+    },
+    install_package_data=True,
+    install_requires=install_requires,
+    license="GPLv3",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    keywords='datgan',
+    name='datgan',
+    packages=find_packages(include=['datgan', 'datgan.*']),
+    python_requires='>=3.5',
+    url='https://github.com/glederrey/DATGAN',
+    version='1.0',
+    zip_safe=False,
+)
