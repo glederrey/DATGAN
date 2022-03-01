a14fd19d82107451c6a5ec4adb2a7ad36a911f04
glederrey <gael.lederrey@epfl.ch>
2022-03-01 20:21:45 +0100

    [UPD] Final setup.py file for release v2

--- setup.py
+++ setup.py
@@ -8,18 +8,15 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.md') as history_file:
-    history = history_file.read()
-
 install_requires = [
-    'numpy>=1.16.0',
-    'pandas>=1.3',
-    'networkx>=2.6',
-    'scikit-learn>=1.0.2',
-    'tensorflow>=2.8.0',
-    'matplotlib>=3.5',
-    'lightgbm>=3.3',
-    'tqdm>=4.62.3'
+    'numpy',
+    'pandas',
+    'networkx',
+    'scikit-learn',
+    'tensorflow==2.8.0',
+    'matplotlib',
+    'lightgbm',
+    'tqdm',
 ]
 
 setup(
@@ -42,7 +39,7 @@ setup(
     install_package_data=True,
     install_requires=install_requires,
     license="GPLv3",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='datgan',
